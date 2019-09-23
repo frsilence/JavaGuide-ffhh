c119035ab0e41349114b2a25594c20bf395b1e29
frsilence <zgqqzyf@163.com>
2019-09-23 23:40:16 +0800

    3

--- JavaSE/1.Java 语法/CODE/Java/cn/ffhh/File/FileDemo.java
+++ JavaSE/1.Java 语法/CODE/Java/cn/ffhh/File/FileDemo.java
@@ -1,9 +1,29 @@
 package cn.ffhh.File;
 
+import java.io.File;
+
 /**
  * @auther FrSilence
  * @date 2019-09-23 15:45
  * Description：文件展示demo
  */
 public class FileDemo {
+    private String path;
+    private File sysFile;
+
+    public FileDemo(String path) {
+        this.path = path;
+    }
+    public void showFilePath(){
+        try{
+            this.sysFile = new File(path);
+        }catch (Exception e){
+            System.out.println("所提供的的目录不合法，提供准确的文件目录");
+        }
+        if(sysFile.isFile()){
+            System.out.println(sysFile.getName());
+        }else{
+            System.out.println(sysFile.getName());
+        }
+    }
 }
---
+++ JavaSE/1.Java 语法/CODE/Java/cn/ffhh/File/TestDemo.java
@@ -0,0 +1,15 @@
+package cn.ffhh.File;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yfzhao
+ * Date: 2019/9/23
+ * Time: 23:15
+ * Description: No Description
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        FileDemo fileDemo = new FileDemo("C:\\Users\\yfzhao\\IdeaProjects\\demo3\\target");
+        fileDemo.showFilePath();
+    }
+}
